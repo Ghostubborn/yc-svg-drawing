ca721c32a50fd6baf14744329f4ec01e37377d14
zhen <ghostubborn@hotmail.com>
2019-02-23 16:10:19 +0800

    添加一些数据，代码添加注释

--- yc-drawing.js
+++ yc-drawing.js
@@ -1,50 +1,58 @@
 SVG.on(document, 'DOMContentLoaded', function () {
   if (SVG.supported) {
-    var width = 1200;
-    var height = 800;
+    var width = 1600;
+    var height = 900;
     var startPosition = { x: 50, y: 0 }
-
     var info = {
       rect: {
-        first: { width: 150, height: 50, margin: 100, fill: '#800000' },
-        others: { width: 150, height: 40, margin: 100, fill: '#0F3763' }
+        firstOrLast: { width: 200, height: 50, margin: 100, fill: '#800000' },
+        others: { width: 200, height: 40, margin: 100, fill: '#0F3763' }
       }
     };
 
     var data = window.ycData;
     var draw = SVG('handmade').size(width, height);
-    draw.style({ background: '#ccf' })
+    // TODO: temp background color
+    draw.style({ background: '#ccf' });
 
-    var domStructure = [];
+    var domStore = [];
 
+    // root nodes loop
     for (var i = 0; i < data.length; i++) {
-      domStructure.push([])
-      if (i === 0) {
-        domStructure[i].rectInfo = info.rect.first;
+      domStore.push([]);
+      if (i === 0 || i === data.length - 1) {
+        domStore[i].rectInfo = info.rect.firstOrLast;
       } else {
-        domStructure[i].rectInfo = info.rect.others;
+        domStore[i].rectInfo = info.rect.others;
       }
-      domStructure[i].rect = draw.rect(domStructure[i].rectInfo.width, domStructure[i].rectInfo.height);
-      domStructure[i].rect.radius(domStructure[i].rectInfo.height / 2);
-      domStructure[i].rect.attr({
-        x: startPosition.x + i * (domStructure[i].rectInfo.width + domStructure[i].rectInfo.margin),
-        y: (height - domStructure[i].rectInfo.height) / 2
-      });
-      domStructure[i].rect.style({ fill: domStructure[i].rectInfo.fill });
 
-      domStructure[i].text = draw.text(data[i].name)
+      // draw root node
+      domStore[i].rect = draw.rect(domStore[i].rectInfo.width, domStore[i].rectInfo.height);
+      domStore[i].rect.radius(domStore[i].rectInfo.height / 2);
+      domStore[i].rect.attr({
+        x: startPosition.x + i * (domStore[i].rectInfo.width + domStore[i].rectInfo.margin),
+        y: (height - domStore[i].rectInfo.height) / 2
+      });
+      domStore[i].rect.style({ fill: domStore[i].rectInfo.fill });
+      domStore[i].text = draw.text(data[i].name)
         .font({ size: '18px' })
         .fill('#fff')
-        .cx(domStructure[i].rect.cx())
-        .cy(domStructure[i].rect.cy());
+        .cx(domStore[i].rect.cx())
+        .cy(domStore[i].rect.cy());
 
+      // line between root nodes
       if (i > 0) {
-        domStructure[i].line = draw.line(
-          domStructure[i - 1].rect.cx() + domStructure[i - 1].rectInfo.width / 2,
-          domStructure[i - 1].rect.cy(),
-          domStructure[i].rect.cx() - domStructure[i].rectInfo.width / 2,
-          domStructure[i].rect.cy()
+        domStore[i].line = draw.line(
+          domStore[i - 1].rect.cx() + domStore[i - 1].rectInfo.width / 2,
+          domStore[i - 1].rect.cy(),
+          domStore[i].rect.cx() - domStore[i].rectInfo.width / 2,
+          domStore[i].rect.cy()
         ).stroke({ width: 1 })
+      }
+
+      // projects invested by nation
+      for (var j = 0; j < data[i].nation.length; j++) {
+
       }
     }
   } else {
