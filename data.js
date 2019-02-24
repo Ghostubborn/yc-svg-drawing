7bc9f0d1068333567c1c8a871a6931715666b595
zhen <ghostubborn@hotmail.com>
2019-02-24 21:00:44 +0800

    画出来了：）

--- data.js
+++ data.js
@@ -68,10 +68,134 @@ window.ycData = [
     ]
   },
   {
-    name: '2016年度'
+    name: '2016年度',
+    nation: [
+      {
+        name: '高效双组份纺粘静电驻极材料的开发',
+        supervisor: '靳向煜',
+        source: '科学技术部',
+        type: '应用技术研究',
+        finance: '80万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2015.1 - 2017.1'
+      },
+      {
+        name: '东华大学高技术纺织品创新技术服务团队',
+        supervisor: '靳向煜',
+        source: '上海市教育委员会',
+        type: '应用基础研究',
+        finance: '60万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2010.4 - 2019.10'
+      }
+    ],
+    enterprise: [
+      {
+        name: '熔喷复合非织造材料制备技术及产业化',
+        supervisor: '靳向煜',
+        partyA: '苍南巨睿印刷材料有限公司',
+        type: '技术服务',
+        contractAmount: '30万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '抗拉强度 < 2000Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2018.9 - 2021.8'
+      },
+      {
+        name: '功能性水刺医卫非织造材料技术及产业化',
+        supervisor: '靳向煜',
+        partyA: '南通通州江华纺织有限公司',
+        type: '技术服务',
+        contractAmount: '100万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2018.11 - 2021.11'
+      },
+    ]
   },
   {
-    name: '2017年度'
+    name: '2017年度',
+    nation: [
+      {
+        name: '高效双组份纺粘静电驻极材料的开发',
+        supervisor: '靳向煜',
+        source: '科学技术部',
+        type: '应用技术研究',
+        finance: '80万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2015.1 - 2017.1'
+      },
+      {
+        name: '东华大学高技术纺织品创新技术服务团队',
+        supervisor: '靳向煜',
+        source: '上海市教育委员会',
+        type: '应用基础研究',
+        finance: '60万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2010.4 - 2019.10'
+      }
+    ],
+    enterprise: [
+      {
+        name: '熔喷复合非织造材料制备技术及产业化',
+        supervisor: '靳向煜',
+        partyA: '苍南巨睿印刷材料有限公司',
+        type: '技术服务',
+        contractAmount: '30万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '抗拉强度 < 2000Mpa',
+          '剪切应力 < 25MPa',
+          '孔隙率不低于 78%',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2018.9 - 2021.8'
+      },
+      {
+        name: '功能性水刺医卫非织造材料技术及产业化',
+        supervisor: '靳向煜',
+        partyA: '南通通州江华纺织有限公司',
+        type: '技术服务',
+        contractAmount: '100万',
+        details: [
+          '屈服强度 > 125Mpa',
+          '抗拉强度 > 1000Mpa',
+          '成本不高于 100/CM'
+        ],
+        timeRange: '2018.11 - 2021.11'
+      },
+    ]
   },
   {
     name: '中远期目标'
--- yc-drawing.js
+++ yc-drawing.js
@@ -1,59 +1,175 @@
 SVG.on(document, 'DOMContentLoaded', function () {
   if (SVG.supported) {
     var width = 1600;
-    var height = 900;
-    var startPosition = { x: 50, y: 0 }
+    var height = 980;
     var info = {
       rect: {
-        firstOrLast: { width: 200, height: 50, margin: 100, fill: '#800000' },
-        others: { width: 200, height: 40, margin: 100, fill: '#0F3763' }
+        firstOrLast: { width: 200, height: 50, fill: '#800000' },
+        others: { width: 200, height: 40, fill: '#0F3763' }
       }
     };
 
     var data = window.ycData;
     var draw = SVG('handmade').size(width, height);
-    // TODO: temp background color
-    draw.style({ background: '#ccf' });
-
-    var domStore = [];
+    var totalBox;
 
     // root nodes loop
+    var rectList = [];
+    var textList = [];
+    var start = { x: 50, cy: height / 2 };
+
     for (var i = 0; i < data.length; i++) {
-      domStore.push([]);
+      var rectInfo;
       if (i === 0 || i === data.length - 1) {
-        domStore[i].rectInfo = info.rect.firstOrLast;
+        rectInfo = info.rect.firstOrLast;
       } else {
-        domStore[i].rectInfo = info.rect.others;
+        rectInfo = info.rect.others;
       }
 
+      rectList.push(draw.rect(rectInfo.width, rectInfo.height));
       // draw root node
-      domStore[i].rect = draw.rect(domStore[i].rectInfo.width, domStore[i].rectInfo.height);
-      domStore[i].rect.radius(domStore[i].rectInfo.height / 2);
-      domStore[i].rect.attr({
-        x: startPosition.x + i * (domStore[i].rectInfo.width + domStore[i].rectInfo.margin),
-        y: (height - domStore[i].rectInfo.height) / 2
-      });
-      domStore[i].rect.style({ fill: domStore[i].rectInfo.fill });
-      domStore[i].text = draw.text(data[i].name)
-        .font({ size: '18px' })
-        .fill('#fff')
-        .cx(domStore[i].rect.cx())
-        .cy(domStore[i].rect.cy());
+      rectList[i].radius(rectInfo.height / 2);
+      if (i === 0) {
+        rectList[i].attr({ x: start.x }).cy(start.cy);
+      } else {
+        rectList[i].attr({ x: rectList[i - 1].bbox().x2 + 50 }).cy(start.cy);
+      }
+      rectList[i].style({ fill: rectInfo.fill });
+      textList.push(draw.text(data[i].name).font({ size: '18px' }).fill('#fff'));
 
       // line between root nodes
       if (i > 0) {
-        domStore[i].line = draw.line(
-          domStore[i - 1].rect.cx() + domStore[i - 1].rectInfo.width / 2,
-          domStore[i - 1].rect.cy(),
-          domStore[i].rect.cx() - domStore[i].rectInfo.width / 2,
-          domStore[i].rect.cy()
-        ).stroke({ width: 1 })
+        drawLine(rectList[i].bbox().x, rectList[i].bbox().cy, rectList[i - 1].bbox().x2, rectList[i - 1].bbox().cy);
       }
 
-      // projects invested by nation
-      for (var j = 0; j < data[i].nation.length; j++) {
-
+      let nationBbox = drawList(
+        formatProjectList(data[i].nation, true),
+        rectList[i].bbox().x + 30,
+        rectList[i].bbox().y,
+        true,
+        true,
+      );
+      let enterpriseBbox = drawList(
+        formatProjectList(data[i].enterprise),
+        rectList[i].bbox().x + 60,
+        rectList[i].bbox().y2,
+        false,
+        true
+      );
+      
+      // reset rect width
+      let yearBbox = rectList[i].bbox();
+      if (nationBbox) {
+        yearBbox = yearBbox.merge(nationBbox);
       }
+      if (enterpriseBbox) {
+        yearBbox = yearBbox.merge(enterpriseBbox);
+      }
+      rectList[i].attr({ width: yearBbox.width });
+      textList[i].cx(rectList[i].cx()).cy(rectList[i].cy());
+
+      if (totalBox) {
+        totalBox = totalBox.merge(yearBbox);
+      } else {
+        totalBox = yearBbox;
+      }
+    }
+
+    // correct position and size
+    draw.viewbox(totalBox.x - 50, totalBox.y, totalBox.width + 100, totalBox.height);
+
+    function drawLine (x, y, x2, y2) {
+      return draw.line(x, y, x2, y2).stroke({ width: 1 });
+    }
+
+    function drawText (x, y, text) {
+      return text = draw.text(text).font({ size: '14px', weight: '400' }).x(x + 15).y(y);
+    }
+
+    function drawList (list, x, y, lowerFirst, isRoot) {
+      if (!list || !list.length) {
+        return false;
+      }
+
+      if (lowerFirst) {
+        list.reverse();
+      }
+
+      let start = { x: x, y: lowerFirst ? y - 25 : y + 9 };
+      let itemBboxList = []
+      let mergedList = undefined;
+
+      for (let i = 0; i < list.length; i++) {
+        // draw text
+        let hasChild = typeof list[i] === 'object';
+        let text = drawText(start.x, start.y, hasChild ? list[i].text : list[i]);
+        let childList;
+        if (hasChild) {
+          // draw list
+          childList = drawList(
+            list[i].children,
+            text.bbox().x2 + 15,
+            text.bbox().y + (lowerFirst ? 25 : 0),
+            lowerFirst
+          );
+          // reset text y position
+          text.cy(childList.cy);
+          // draw line at right of text
+          drawLine(text.bbox().x2 + 15, text.bbox().cy, text.bbox().x2 + 5, text.bbox().cy)
+        }
+
+        // draw line at left of text
+        let leftLine = drawLine(text.bbox().x - 15, text.bbox().cy, text.bbox().x - 5, text.bbox().cy)
+
+        // save whole item bbox
+        if (childList) {
+          itemBboxList.push(text.bbox().merge(childList).merge(leftLine.bbox()));
+        } else {
+          itemBboxList.push(text.bbox().merge(leftLine.bbox()));
+        }
+
+        // draw vertical line from current item to the previous one
+        if (i > 0) {
+          drawLine(itemBboxList[i].x, itemBboxList[i].cy, itemBboxList[i - 1].x, itemBboxList[i - 1].cy);
+        } else if (isRoot) {
+          drawLine(itemBboxList[i].x, itemBboxList[i].cy, x, y);
+        }
+
+        // set y position from where next item starts
+        start.y = lowerFirst ? (itemBboxList[i].y - 25) : (itemBboxList[i].y2 + 9);
+
+        // merge current item to list
+        if (mergedList) {
+          mergedList = mergedList.merge(itemBboxList[i]);
+        } else {
+          mergedList = itemBboxList[i];
+        }
+      }
+      return mergedList;
+    }
+
+    function formatProjectList (data, isNation) {
+      if (!data || !data.length) {
+        return;
+      }
+      var keys = isNation ?
+        ['name', 'supervisor', 'source', 'type', 'finance', 'details', 'timeRange'] :
+        ['name', 'supervisor', 'partyA', 'type', 'contractAmount', 'details', 'timeRange'];
+      return data.map((item, index) => {
+        return {
+          text: `${isNation ? '纵向研发' : '横向研发'} ${index + 1}`,
+          children: keys.map(key => {
+            if (key !== 'details') {
+              return item[key];
+            } else {
+              return {
+                text: '技术指标',
+                children: item[key]
+              }
+            }
+          })
+        }
+      })
     }
   } else {
     alert('浏览器不支持SVG')
